fc14805cc8c41593ed00231000dae24b3d150df0
PhucLoc <longphucloc123@gmail.com>
2024-06-15 15:57:27 +0700

    add interface map to address screen

--- app/(tabs)/home/address.js
+++ app/(tabs)/home/address.js
@@ -1,19 +1,30 @@
-import { Pressable, StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { Pressable, ScrollView, StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
 import {
   Entypo,
-  Ionicons,
+  Ionicons, AntDesign, FontAwesome, EvilIcons
 } from "@expo/vector-icons";
 
 const address = () => {
+  const [step, setStep] = useState(1);
 
   const handleBack = () => {
-
-  }
+    setStep((prevStep) => (prevStep > 1 ? prevStep - 1 : prevStep));
+  };
 
   const handleNext = () => {
-    
-  }
+    setStep((prevStep) => {
+      const nextStep = prevStep +1;
+      console.log('next step ', nextStep);
+
+      //check if next step is equal to 4
+      if (nextStep == 5) {
+        // Call the place order function
+      }
+      return nextStep;
+    });
+  };
+  console.log(step);
 
   return (
     <View style={styles.container}>
@@ -45,13 +56,62 @@ const address = () => {
         </Pressable>
       </View>
 
+      <View style={styles.mapContainer}>
+        <ScrollView>
+          {
+            step == 1 && (
+              <View>
+                {/* Map over all the address */}
+                <Pressable style={styles.map_iconButton}>
+                  <AntDesign name="plus" size={24} color='black' />
+                  <Pressable>
+                    <Text style={{fontSize:16}}>Add address</Text>
+                  </Pressable>
+                </Pressable>
+
+                {/* Map over the address */}
+                <View>
+                  <Pressable style={styles.map_addressInput}>
+                    <View style={styles.map_addressInput_contain}>
+                      <View style={styles.map_iconButton}>
+                        <Ionicons name="location-outline" size={24} color='#0066b2' />
+                        <Text style={{fontSize:17,fontWeight:'500'}}>Home</Text>
+                      </View>
+                      <FontAwesome name="flag" size={22} color='#0066b2' />
+                    </View>
+
+                    <Text style={styles.map_addressInput_textName}>
+                      #21 Viet Nam in the world Viet Nam in the world Viet Nam in the world Viet Nam in the world {" "}
+                    </Text>
+                    <Text style={styles.map_addressInput_textTitle}>Ho Chi Minh city</Text>
+                  </Pressable>
+                </View>
+              </View>
+            )
+          }
+
+          {
+            step == 2 && (
+              <View>
+                <View>
+                  <EvilIcons name="location" size={24} color='black' />
+                  <View>
+                    <Text>Pick up slot</Text>
+                  </View>
+                </View>
+              </View>
+            )
+          }
+        </ScrollView>
+      </View>
+
       <View style={styles.footer}>
-        <Pressable style={[styles.footer_button, {backgroundColor:'#d0d0d0'}]} onPress={handleBack}>
+        <Pressable style={[styles.footer_button, {backgroundColor:'#d0d0d0'}]} onPress={handleBack} disabled={step===1}>
             <Text style={styles.footer_button_text}>Back</Text>
         </Pressable>
 
         <Pressable style={[styles.footer_button, {backgroundColor:'#0066b2'}]} onPress={handleNext}>
-            <Text style={[styles.footer_button_text, {color:'white'}]}>Next</Text>
+            <Text style={[styles.footer_button_text, {color:'white'}]}>{step == 4 ? 'Place Order' : 'Next'}</Text>
         </Pressable>
       </View>
     </View>
@@ -124,6 +184,41 @@ const styles = StyleSheet.create({
   },
   footer_button_text:{
     textAlign:'center',
+    fontWeight:'500'
+  },
+  mapContainer:{
+    backgroundColor:'#f0f8ff',
+    flex:1,
+    padding:10
+  },
+  map_iconButton:{
+    flexDirection:'row',
+    alignItems:'center',
+    gap:10
+  },
+  map_addressInput:{
+    backgroundColor:'white',
+    padding:10,
+    marginVertical:10,
+    borderRadius:15,
+    borderWidth:1,
+    borderColor:'#0066b2'
+  },
+  map_addressInput_contain:{
+    flexDirection:'row',
+    alignItems:'center',
+    justifyContent:'space-between'
+  },
+  map_addressInput_textName:{
+    marginTop:10,
+    fontSize:15,
+    fontWeight:'500',
+    width:'95%'
+  },
+  map_addressInput_textTitle:{
+    marginTop:6,
+    color:'gray',
+    fontSize:15,
     fontWeight:'500'
   }
 });
